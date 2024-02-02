aeb01d457229e37b63230cbeca77b15eb13b6a3d
Sheoak <sheoak@users.noreply.github.com>
2024-02-02 11:31:45 +0700

    fix: fix notice on missing parameter

--- tests/app_fields.py
+++ tests/app_fields.py
@@ -5,11 +5,11 @@ from omemdb import Record, Db
 class CustomFieldsRecord(Record):
     class Schema(MarshSchema):
         pk = fields.Int(required=True)
-        date_time = fields.DateTime(allow_none=True, missing=None)
-        date = fields.Date(allow_none=True, missing=None)
-        time = fields.Time(allow_none=True, missing=None)
-        time_delta = fields.TimeDelta(allow_none=True, missing=None)
-        numpy_array = fields.NumpyArray(allow_none=True, missing=None)
+        date_time = fields.DateTime(allow_none=True, load_default=None)
+        date = fields.Date(allow_none=True, load_default=None)
+        time = fields.Time(allow_none=True, load_default=None)
+        time_delta = fields.TimeDelta(allow_none=True, load_default=None)
+        numpy_array = fields.NumpyArray(allow_none=True, load_default=None)
 
     class TableMeta:
         pass
@@ -27,14 +27,14 @@ class AllowNoneFieldRecord(Record):
     """
     allow_none documentation:
     Set this to True if None should be considered a valid value during validation/deserialization.
-    If missing=None and allow_none is unset, will default to True. Otherwise, the default is False.
+    If load_default=None and allow_none is unset, will default to True. Otherwise, the default is False.
     """
     class Schema(MarshSchema):
         ref = fields.RefField(required=True)
-        can_be_none = fields.Int(allow_none=True, missing=None)
-        cant_be_none = fields.Int(allow_none=False, missing=0)
-        default_can_be_none = fields.Int(allow_none=True, missing=None)
-        default_cant_be_none = fields.Int(missing=0)
+        can_be_none = fields.Int(allow_none=True, load_default=None)
+        cant_be_none = fields.Int(allow_none=False, load_default=0)
+        default_can_be_none = fields.Int(allow_none=True, load_default=None)
+        default_cant_be_none = fields.Int(load_default=0)
 
 
 class AppFields(Db):
